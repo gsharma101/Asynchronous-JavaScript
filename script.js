18e0f4ebdf5425b362bdbe4c435fb5d06248bf3b
unknown <sharmag8121@gmail.com>
2023-08-15 00:47:16 +0530

    call back hell

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 // ? first Ajax
-
+/*
 const getCountryData = function (country) {
   // ? XML Object
   const request = new XMLHttpRequest();
@@ -16,15 +16,15 @@ const getCountryData = function (country) {
     console.log(data);
 
     const html = ` <article class="country">
-		    <img class="country__img" src="${data.flag}" />
-		    <div class="country__data">
-		      <h3 class="country__name">${data.name}</h3>
-		      <h4 class="country__region">${data.region}</h4>
-		      <p class="country__row"><span>👫</span>${(data.population / 1000000).toFixed(1)}</p>
-		      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-		      <p class="country__row"><span>💰</span>${data.languages[0].name}</p>
-		    </div>
-		  </article>`;
+        <img class="country__img" src="${data.flag}" />
+        <div class="country__data">
+          <h3 class="country__name">${data.name}</h3>
+          <h4 class="country__region">${data.region}</h4>
+          <p class="country__row"><span>👫</span>${(data.population / 1000000).toFixed(1)}</p>
+          <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+          <p class="country__row"><span>💰</span>${data.languages[0].name}</p>
+        </div>
+      </article>`;
 
     countriesContainer.insertAdjacentHTML('beforeend', html);
     countriesContainer.style.opacity = 1;
@@ -32,3 +32,54 @@ const getCountryData = function (country) {
 }
 
 getCountryData('Republic of India');
+*/
+
+const renderCountry = function (data, className = '') {
+  const html = ` <article class="country ${className}">
+        <img class="country__img" src="${data.flag}" />
+        <div class="country__data">
+          <h3 class="country__name">${data.name}</h3>
+          <h4 class="country__region">${data.region}</h4>
+          <p class="country__row"><span>👫</span>${(data.population / 1000000).toFixed(1)}</p>
+          <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+          <p class="country__row"><span>💰</span>${data.languages[0].name}</p>
+        </div>
+      </article>`;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+}
+
+const getCountryAndNeighbour = function (country) {
+  // ? AJAX call country 1
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    //? Render Country 1
+    renderCountry(data);
+
+    //? Get neighbour country 2
+
+    const [neighbour] = data.borders;
+
+    if (!neighbour) return;
+
+    // ? AJAX call country 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const data2 = JSON.parse(this.responseText);
+
+      renderCountry(data2, 'neighbour');
+    });
+  });
+}
+
+getCountryAndNeighbour('Republic of India');
