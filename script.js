06854d0549d93f404230298c8c9cdec042c325b2
unknown <sharmag8121@gmail.com>
2023-08-16 00:57:36 +0530

    Rejected Promises 2

--- script.js
+++ script.js
@@ -3,6 +3,27 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+  // countriesContainer.style.opacity = 1;
+}
+
+const renderCountry = function (data, className = '') {
+  const html = ` <article class="country ${className}">
+        <img class="country__img" src="${data.flag}" />
+        <div class="country__data">
+          <h3 class="country__name">${data.name}</h3>
+          <h4 class="country__region">${data.region}</h4>
+          <p class="country__row"><span>👫</span>${(data.population / 1000000).toFixed(1)}</p>
+          <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+          <p class="country__row"><span>💰</span>${data.languages[0].name}</p>
+        </div>
+      </article>`;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  // countriesContainer.style.opacity = 1;
+}
+
 // ? first Ajax
 /*
 const getCountryData = function (country) {
@@ -85,21 +106,6 @@ const getCountryAndNeighbour = function (country) {
 getCountryAndNeighbour('Republic of India');
 */
 
-const renderCountry = function (data, className = '') {
-  const html = ` <article class="country ${className}">
-        <img class="country__img" src="${data.flag}" />
-        <div class="country__data">
-          <h3 class="country__name">${data.name}</h3>
-          <h4 class="country__region">${data.region}</h4>
-          <p class="country__row"><span>👫</span>${(data.population / 1000000).toFixed(1)}</p>
-          <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-          <p class="country__row"><span>💰</span>${data.languages[0].name}</p>
-        </div>
-      </article>`;
-
-  countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = 1;
-}
 /*
 const getCountryData = function (country) {
   fetch(`https://restcountries.com/v2/name/${country}`)
@@ -127,10 +133,17 @@ const getCountryData = function (country) {
     })
     .then(response => response.json())
     .then(data => renderCountry(data, 'neighbour'))
-    .catch(err => alert(err));
+    .catch(err => {
+      renderError(`Something went wrong ${err.message}. Try again!`);
+    })
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
+    })
 }
 
 
 btn.addEventListener('click', function () {
   getCountryData('Republic of India');
 });
+
+getCountryData('sdifjsoidjfsoidjf');
